e4c37c7bd17434464bcb1e146eb1c27e3bbf14e0
Tarik Amehri <tamehri@e3r1p4.1337.ma>
2024-08-16 15:08:20 +0100

    cpp08/ex02 started

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -3,21 +3,35 @@
 
 # include <iostream>
 # include <algorithm>
+# include <deque>
 # include <stack>
 # include <list>
 
-
-template <typename Type>
+template <typename Type, class Container = std::deque<Type> >
 class MutantStack : public std::stack<Type>
 {
 	public:
+		typedef typename std::deque<Type>::iterator					iterator;
+		typedef typename std::deque<Type>::const_iterator			const_iterator;
+		typedef typename std::deque<Type>::reverse_iterator			reverse_iterator;
+		typedef typename std::deque<Type>::const_reverse_iterator	const_reverse_iterator;
+
 		MutantStack() { }
-		MutantStack( const MutantStack &copy ) { (void)copy; }
+		MutantStack( const MutantStack &copy ) { *this = copy; }
 		MutantStack	&operator=( const MutantStack &assign ) {
-			(void)assign;
+			std::stack<Type>operator=( assign );
 			return *this;
 		}
 		~MutantStack() { }
+
+		iterator				end( void ) { return this->c.end(); }
+		iterator				begin( void ) { return this->c.begin(); }
+		const_iterator			cend( void ) const { return this->c.cend(); }
+		const_iterator			cbegin( void ) const { return this->c.cbegin(); }
+		reverse_iterator		rend( void ) { return this->c.rend(); }
+		reverse_iterator		rbegin( void ) { return this->c.rbegin(); }
+		const_reverse_iterator	crend( void ) const { return this->c.crend(); }
+		const_reverse_iterator	crbegin( void ) const { return this->c.crbegin(); }
 };
 
 #endif
